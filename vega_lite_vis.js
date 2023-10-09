e9eed8a2609e6565956d03123b2d961f624b285d
gojoizgod <142491463+gojoizgod@users.noreply.github.com>
2023-10-10 02:16:12 +1100

    Add files via upload

---
+++ vega_lite_vis.js
@@ -0,0 +1,32 @@
+var vg_1 = "map2.json";
+vegaEmbed("#map_chart", vg_1).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+var vg_2 = "areachart.json";
+vegaEmbed("#area_chart", vg_2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+var vg_3 = "wounded.json";
+vegaEmbed("#area_chart2", vg_3).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+var vg_4 = "donut1.json";
+vegaEmbed("#donut1", vg_4).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+var vg_5 = "donut2.json";
+vegaEmbed("#donut2", vg_5).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+var vg_6 = "donut3.json";
+vegaEmbed("#donut3", vg_6).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+var vg_7 = "rotate.json";
+vegaEmbed("#rot", vg_7).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+var vg_8 = "orgys.json";
+vegaEmbed("#orgy", vg_8).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
